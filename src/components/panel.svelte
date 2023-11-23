8dd399b3385aabe33534fdcf593101b4f0433ba6
Luto Yvan <zuoez02@outlook.com>
2023-11-23 22:33:36 +0800

    v0.1.0: first version

---
+++ src/components/panel.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    import { Protyle } from "siyuan";
+    import { onMount } from "svelte";
+    import DockyPlugin from "..";
+
+    let block;
+
+    onMount(() => {
+        new Protyle(plugin.app, block, {
+            blockId: blockId,
+            render: {
+                background: false,
+                title: false,
+                gutter: false,
+                scroll: true,
+                breadcrumb: false,
+                breadcrumbDocName: false,
+            }
+        });
+    })
+
+    export let blockId: string;
+    export let plugin: DockyPlugin;
+</script>
+
+<div bind:this={block}></div>
